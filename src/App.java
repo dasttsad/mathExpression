5e00cce0d2f0fe43bb0f45eb8c551ae448f1f8c5
Stella <stelladagostino5@gmail.com>
2025-05-31 15:09:08 +0200

    goal

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        /*
+         * obiettivo: 
+         * generare un'espressione casuale e far sì che l'app controlli che il risultato fornito dall'utente sia corretto
+         */
+        
+    }
+}
