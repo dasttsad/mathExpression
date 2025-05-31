55aa7c4b9243d2b04358948382f1604d34ef5eca
Stella <stelladagostino5@gmail.com>
2025-05-31 15:10:58 +0200

    random numbers

--- src/App.java
+++ src/App.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         
@@ -5,6 +8,26 @@ public class App {
          * obiettivo: 
          * generare un'espressione casuale e far sì che l'app controlli che il risultato fornito dall'utente sia corretto
          */
-        
+
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
+
+        // generare quanti numeri avrà l'espressione, nel mio caso sarà da 2 a 4
+        int length = random.nextInt(3)+2;
+
+        // generare i numeri dell'espressione (a prescindere dalla length, ne faccio generare 4)
+        int first = random.nextInt(99)+1;
+        int second = random.nextInt(99)+1;
+        int third = random.nextInt(99)+1;
+        int fourth = random.nextInt(99)+1;
+
+        // controllo per capire se funziona
+        if (length == 2) {
+            System.out.println(first + "," + second);
+        } else if (length == 3) {
+            System.out.println(first + "," + second + "," + third);
+        } else {
+            System.out.println(first + "," + second + "," + third + "," + fourth);
+        }
     }
 }
